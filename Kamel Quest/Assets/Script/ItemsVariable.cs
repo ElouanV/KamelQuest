8fd2714c6ab3d45165e6fe508572989a60b164df
ElouanV <elouan.vincent@epita.fr>
2020-03-11 00:27:16 +0100

    Commit pitié

--- Kamel Quest/Assets/Script/ItemsVariable.cs
+++ Kamel Quest/Assets/Script/ItemsVariable.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemVariable : MonoBehaviour
+public class ItemsVariable : MonoBehaviour
 {
-    public string itemType;
+    //public Text textItem;
+    public GameObject textDisplay;
+    [Header("Item's Data")]
+    public string itemName;
     public string itemID;
     public Sprite itemSprite;
     public string itemDescription;
+    public int count;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
